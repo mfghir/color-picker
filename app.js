35222af982033522e6fe17e939c6f5112ed6e2c4
miss-f-g <fatemeghafari98@gmail.com>
2022-07-16 17:21:45 +0430

    10.write resetInputs func

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ function randomColors() {
 
     colorizeSliders(color, hue, brightness, saturation);
   });
+  resetInputs();
 }
 
 function checkTextContrast(color, text) {
@@ -105,5 +106,26 @@ function updateTextUI(index) {
     checkTextContrast(color, icon);
   }
 }
+
+function resetInputs() {
+    const sliders = document.querySelectorAll(".sliders input");
+    sliders.forEach(slider => {
+      if (slider.name === "hue") {
+        const hueColor = initialColors[slider.getAttribute("data-hue")];
+        const hueValue = chroma(hueColor).hsl()[0];
+        slider.value = Math.floor(hueValue);
+      }
+      if (slider.name === "brightness") {
+        const brightColor = initialColors[slider.getAttribute("data-bright")];
+        const brightValue = chroma(brightColor).hsl()[2];
+        slider.value = Math.floor(brightValue * 100) / 100;
+      }
+      if (slider.name === "saturation") {
+        const satColor = initialColors[slider.getAttribute("data-sat")];
+        const satValue = chroma(satColor).hsl()[1];
+        slider.value = Math.floor(satValue * 100) / 100;
+      }
+    });
+  }
 
 randomColors();
